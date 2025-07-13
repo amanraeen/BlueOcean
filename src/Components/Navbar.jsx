70800bca30f1afae54088dfa51e02fe565dd3612
Mohammd Aman Malik <amankgm108@gmail.com>
2025-07-13 14:42:59 +0530

    Hide ScrollBar and make homepage responsive

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,16 +3,16 @@ import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="max-w-fit px-10 py-5 h-screen border-r-2  border-r-gray-400  bg-amber-100">
+    <div className="w-full p-3 h-screen border-r-2  border-r-gray-400  bg-amber-100">
 
       {/* <div className="flex flex-col h-full background justify-between"> */}
       <div className="h-full">
         {/* logo section  */}
-        <div className="m-2 border-b-2 border-dotted  w-full border-b-gray-400">
-          <Link to={'/'}><h1 className="pb-1 font-bold text-2xl"> <span className="text-sky-600 ">Blue</span>Ocean</h1></Link>
+        <div className=" border-b-2 border-dotted  w-full border-b-gray-400">
+          <Link to={'/'}><h1 className="text-center pb-1 font-bold text-[1.25em] md:text-2xl"> <span className="text-sky-600 ">Blue</span>Ocean</h1></Link>
           </div>
 
-        <nav className="flex flex-col justify-around w-fit text-[1.1em]  h-full">
+        <nav className="flex flex-col justify-around w-fit md:text-[1.1em]  h-full">
         <ul className="flex flex-col gap-4 ">
             <NavLink to={'/'} className={({isActive})=>`${isActive?"background-2":''} mr-auto px-4 py-1`}><li>Home</li></NavLink>
             <NavLink to={'/explore'} className={({isActive})=>`${isActive?"background-2 ":''} mr-auto  px-4 py-1`}><li>Explore</li></NavLink>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     <>
       <div className="max-w-full grid grid-cols-12 ">
 
-        <div className="col-span-2 w-full">
+        <div className=" sticky col-span-2 w-full hidden sm:block">
           
            <Navbar />
         
@@ -14,7 +14,7 @@ const Home = () => {
 
 
 
-        <div className="col-span-8 flex flex-col border-l-2 border-r-2  border-r-gray-400 border-l-gray-400 h-screen bg-amber-100">
+        <div className="col-span-12 sm:col-span-10 lg:col-span-8 flex flex-col h-screen bg-amber-100">
             <div className=" m-4 p-4  text-xl text-center bg-sky-400">Top Heading</div>
           <div className="grid grid-cols-6 p-3 justify-around gap-4 w-full ">
             <div className="col-span-3 bg-sky-300">Content 1</div>
@@ -23,7 +23,7 @@ const Home = () => {
         </div>
 
 
-        <div className="col-span-2 flex justify-center bg-blue-100">
+        <div className="sticky col-span-2 flex justify-center bg-blue-100 hidden md:block">
           <div className="flex justify-center mx-auto ">
             <h2>CONTENT</h2>  
           </div>
